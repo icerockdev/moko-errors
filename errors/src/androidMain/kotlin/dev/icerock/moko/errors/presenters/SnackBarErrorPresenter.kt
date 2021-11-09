b5c72c65afd27e186e3b994b069972f13c24a678
Aleksey Mikhailov <Aleksey.Mikhailov@icerockdev.com>
2021-11-09 21:30:52 +0700

    #26 fix snackbar showing

--- errors/src/androidMain/kotlin/dev/icerock/moko/errors/presenters/SnackBarErrorPresenter.kt
+++ errors/src/androidMain/kotlin/dev/icerock/moko/errors/presenters/SnackBarErrorPresenter.kt
@@ -5,7 +5,9 @@
 package dev.icerock.moko.errors.presenters
 
 import android.view.View
+import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.FragmentManager
 import com.google.android.material.snackbar.Snackbar
 import dev.icerock.moko.resources.desc.StringDesc
 
@@ -14,16 +16,30 @@ actual class SnackBarErrorPresenter actual constructor(
 ) : ErrorPresenter<StringDesc> {
 
     override fun show(throwable: Throwable, activity: FragmentActivity, data: StringDesc) {
-        val rootView = activity.findViewById<View>(android.R.id.content)
-            ?: activity.window?.decorView?.findViewById<View>(android.R.id.content)
-        if (rootView != null) {
-           val snackbar = Snackbar.make(
-                rootView,
-                data.toString(activity),
-                duration.toAndroidCode()
-            )
-            snackbar.anchorView = activity.actionBar?.customView
-            snackbar.show()
-        }
+        val decorView: View = if (activity.hasWindowFocus()) {
+            activity.window?.decorView
+        } else {
+            val dialogFragment = activity.supportFragmentManager.findActiveDialogFragment()
+            dialogFragment?.dialog?.window?.decorView
+        } ?: return
+        val contentView: View = decorView.findViewById(android.R.id.content) ?: return
+        val snackbar = Snackbar.make(
+            contentView,
+            data.toString(activity),
+            duration.toAndroidCode()
+        )
+        snackbar.show()
+    }
+
+    private fun FragmentManager.findActiveDialogFragment(): DialogFragment? {
+        val dialogFragment: DialogFragment? = fragments.filterIsInstance<DialogFragment>()
+            .filter { it.showsDialog }
+            .filter { it.isResumed }
+            .filter { it.dialog?.window?.decorView?.hasWindowFocus() == true }
+            .firstOrNull()
+
+        if (dialogFragment != null) return dialogFragment
+
+        return fragments.firstNotNullOfOrNull { it.childFragmentManager.findActiveDialogFragment() }
     }
 }
